ec6ffdb26fe32a3a163125a86d3deb9fb2184ce4
halawaoji97 <ozidhalawa97@gmail.com>
2022-06-11 12:41:13 +0700

    finish

--- src/components/DefaultPage.vue
+++ src/components/DefaultPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <Transition name="fade">
-    <div class="container shadow-lg p-3 mb-5 bg-gray rounded mt-5">
+  <div class="container shadow-lg p-3 mb-5 bg-gray rounded mt-5">
+    <transition name="fade" mode="out-in">
       <div class="row">
         <div class="col-12">
           <slot name="title" />
@@ -8,8 +8,8 @@
           <slot />
         </div>
       </div>
-    </div>
-  </Transition>
+    </transition>
+  </div>
 </template>
 <script>
 import { defineComponent, Transition } from 'vue';
